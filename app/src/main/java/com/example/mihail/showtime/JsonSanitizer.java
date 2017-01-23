a0ddb8abc9b1fe4e20732cdd32792da29551cfe2
MihailTrajkovski <mhltrajkovski@gmail.com>
2017-01-23 02:09:37 +0100

    Lates

--- app/src/main/java/com/example/mihail/showtime/JsonSanitizer.java
+++ app/src/main/java/com/example/mihail/showtime/JsonSanitizer.java
@@ -10,15 +10,4 @@ public class JsonSanitizer {
         return wellFormedJson;
     }
 
-    protected static String jsonSanitizeTwo(String jsonString) {
-        String wellFormedJson = com.google.json.JsonSanitizer.sanitize(jsonString);
-        String responseThree = jsonSanitizeThree(jsonString);
-        System.out.println(responseThree);
-        return wellFormedJson;
-    }
-
-    private static String jsonSanitizeThree(String jsonString) {
-        return jsonString;
-    }
-
 }
--- app/src/main/java/com/example/mihail/showtime/LoginDataBaseAdapter.java
+++ app/src/main/java/com/example/mihail/showtime/LoginDataBaseAdapter.java
@@ -55,14 +55,14 @@ public class LoginDataBaseAdapter
         db.insert("LOGIN", null, newValues);
         ///Toast.makeText(context, "Reminder Is Successfully Saved", Toast.LENGTH_LONG).show();
     }
-    public int deleteEntry(String UserName)
+    /*public int deleteEntry(String UserName)
     {
         //String id=String.valueOf(ID);
         String where="USERNAME=?";
         int numberOFEntriesDeleted= db.delete("LOGIN", where, new String[]{UserName}) ;
         // Toast.makeText(context, "Number fo Entry Deleted Successfully : "+numberOFEntriesDeleted, Toast.LENGTH_LONG).show();
         return numberOFEntriesDeleted;
-    }
+    }*/
     public String getSinlgeEntry(String userName)
     {
         Cursor cursor=db.query("LOGIN", null, " USERNAME=?", new String[]{userName}, null, null, null);
@@ -76,7 +76,7 @@ public class LoginDataBaseAdapter
         cursor.close();
         return password;
     }
-    public void  updateEntry(String userName,String password)
+    /*public void  updateEntry(String userName,String password)
     {
         // Define the updated row content.
         ContentValues updatedValues = new ContentValues();
@@ -86,5 +86,5 @@ public class LoginDataBaseAdapter
 
         String where="USERNAME = ?";
         db.update("LOGIN",updatedValues, where, new String[]{userName});
-    }
+    }*/
 }
